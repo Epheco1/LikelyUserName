030e184764a61e77c027a5dd4346b0d3f1544475
Epheco1 <Epheco1@WEMAITS>
2022-08-20 22:27:53 +0100

    Add project files.

---
+++ LikelyUserName/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LikelyUserName
+{
+    
+    internal class Program
+    {
+        public delegate bool LikelyUsername(Name name);
+        static void Main(string[] args)
+        {
+            LikelyUsername dUserCount = delegate (Name name)
+            {
+                return name.UserName == (name.likelyUserName = name.FirstName + "." + name.LastName);
+            };
+
+            Name personOne = new Name();
+            personOne.FirstName = "Ephraim";
+            personOne.LastName = "Usoroh";
+            personOne.UserName = "Ephraim.Usoroh";
+            Console.WriteLine(dUserCount(personOne));
+        }
+
+    }
+}
